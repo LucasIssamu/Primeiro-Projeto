6ffd7f6ec3460f3df5a7c219873358d6674cbfea
Lucas Issamu <lucas.sakugawa@fatec.sp.gov.br>
2022-02-22 20:34:36 -0300

    criacao do layout

---
+++ primeiroprojeto/resources/views/exercicio1.blade.php
@@ -0,0 +1,10 @@
+@extends('layout')
+
+@section('titulo')
+    primeiro exercicio
+@endsection
+
+@section('conteudo')
+    <p> No sabado vou fazer esse exercicio</p>
+
+@endsection
--- primeiroprojeto/resources/views/welcome.blade.php
+++ primeiroprojeto/resources/views/welcome.blade.php
@@ -7,9 +7,10 @@
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-
+    <title>@yield('titulo')</title>
 </head>
 <body>
+<h2>@yield('conteudo')</h2>
 <nav class="navbar navbar-expand-lg navbar-dark bg-primary pl-5 pr-5">
     <div class="container-fluid">
         <a href="#">
