163af34e0f917c704f2ebb668e0f596b2680380e
Lucas Issamu <lucas.sakugawa@fatec.sp.gov.br>
2022-02-22 19:18:45 -0300

    logotipos

--- primeiroprojeto/resources/views/welcome.blade.php
+++ primeiroprojeto/resources/views/welcome.blade.php
@@ -7,14 +7,15 @@
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
 </head>
 <body>
-<nav class="navbar navbar-expand-lg navbar-dark bg-primary pl-5 pr-5 mb-5">
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary pl-5 pr-5">
     <div class="container-fluid">
-        <!--<a href="#">
-            <img src="images/strike.png" alt="logo"/>
-        </a> -->
-        <a class="navbar-brand" href="#">Minha Loja</a>
+        <a href="#">
+            <img src="images/logo2.png" alt="logo" width="140" height="140"/>
+        </a>
+        <!--<a class="navbar-brand" href="#">Minha Loja</a> -->
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
@@ -42,11 +43,10 @@
                         <li><a class="dropdown-item" href="#">Calças</a></li>
                         <li><a class="dropdown-item" href="#">Calçados</a></li>
                         <li><a class="dropdown-item" href="#">Acessórios</a></li>
-                        <!--<li><hr class="dropdown-divider"></li>
-                        <li><a class="dropdown-item" href="#">Something else here</a></li> -->
                     </ul>
                 </li>
             </ul>
+            <i class="fa fa-shopping"></i>
             <form class="d-flex">
                 <input class="form-control me-2" type="search" placeholder="Pesquisar" aria-label="Search">
                 <button class="btn btn-outline-light" type="submit">Pesquisar</button>
@@ -55,14 +55,16 @@
     </div>
 </nav>
 
+<section class="bg-dark p-5">
     <div class="container">
-        <div class="row">
+        <div class="row text-center">
             <div class="col-3 mb-3">
-                <div class="card">
+                <div class="card bg-secondary text-light border-warning">
                     <img src="images/produto1.jpg" alt="produto1" class="card-img-top">
                     <div class="card-body">
                         <h6 class="card-title">Produto 1</h6>
-                        <a href="#" class="btn btn-sm btn-secondary">Adicionar</a>
+                        <p class="card-text"> R$0,00</p>
+                        <a href="#" class="btn btn-sm btn-warning justify-center">Adicionar</a>
                     </div>
                 </div>
             </div>
@@ -72,6 +74,7 @@
                     <img src="" alt="" class="card-img-top">
                     <div class="card-body">
                         <h6 class="card-title">Produto 2</h6>
+                        <p class="card-text"> R$0,00</p>
                         <a href="#" class="btn btn-sm btn-secondary">Adicionar</a>
                     </div>
                 </div>
@@ -82,6 +85,7 @@
                     <img src="" alt="" class="card-img-top">
                     <div class="card-body">
                         <h6 class="card-title">Produto 3</h6>
+                        <p class="card-text"> R$0,00</p>
                         <a href="#" class="btn btn-sm btn-secondary">Adicionar</a>
                     </div>
                 </div>
@@ -92,17 +96,14 @@
                     <img src="" alt="" class="card-img-top">
                     <div class="card-body">
                         <h6 class="card-title">Produto 4</h6>
+                        <p class="card-text"> R$0,00</p>
                         <a href="#" class="btn btn-sm btn-secondary">Adicionar</a>
                     </div>
                 </div>
             </div>
-
-
-
-
         </div>
     </div>
-
+</section>
 <!-- Optional JavaScript; choose one of the two! -->
 
 <!-- Option 1: Bootstrap Bundle with Popper -->
